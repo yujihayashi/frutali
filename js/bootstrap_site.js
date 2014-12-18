59c8ae14f394386f1955b00b5b5bfb0e10b9ab14
Yuji Hayashi <arthuryuji@hotmail.com>
2014-12-18 14:56:40 -0300

    Iniciando diagramacao

--- js/bootstrap_site.js
+++ js/bootstrap_site.js
@@ -6,8 +6,15 @@ $(function() {
     $("#load").ajaxStop(function() {
         $(this).hide();
     });
+    // INTERAÇÃO COM AS BANDEIRAS NO TOPO POR ENQUANTO SÃO ANIMAÇÕES FIXAS
+    $('.lang').hover(function () {
+        $(this).find('ul > li:nth-child(2)').stop(true, true).delay(400).animate({top: '40px'}, {duration: 600, queue: true, easing: 'easeInOutExpo'});
+        $(this).find('ul > li:nth-child(3)').stop(true, true).delay(400).animate({top: '80px'}, {duration: 600, queue: true, easing: 'easeInOutExpo'});
+    }, function () {
+        $(this).find('ul > li:nth-child(2)').stop(true, true).delay(400).animate({top: '0px'}, {duration: 600, queue: true, easing: 'easeInOutExpo'});
+        $(this).find('ul > li:nth-child(3)').stop(true, true).delay(400).animate({top: '0px'}, {duration: 600, queue: true, easing: 'easeInOutExpo'});
+    });
 
-    
 });
 $(function() {
     $('.jImgFormat').find('img').each(function() {
@@ -16,13 +23,13 @@ $(function() {
         var margin = '';
         switch ($float) {
             case 'left':
-                margin = 'margin:2px 10px 2px 0px';
-                break;
+            margin = 'margin:2px 10px 2px 0px';
+            break;
             case 'right':
-                margin = 'margin:2px 0px 2px 10px';
-                break;
+            margin = 'margin:2px 0px 2px 10px';
+            break;
             default:
-                break;
+            break;
         }
         var url_amp = $(this).attr('src');
         url_amp = url_amp.replace(/-([0-9]*)\./, '.');
@@ -34,15 +41,15 @@ $(function() {
 function showPopup() {
     $('.popup').fadeIn('slow');
     var popupMarginLeft = $('.popup-img').width() / 2,
-        popupMarginTop = $('.popup-img').height() / 2;
+    popupMarginTop = $('.popup-img').height() / 2;
 
     // console.log(popupMarginLeft);
     // console.log($('.popup-img img').attr('height'));
-    $('.popup-content').css({marginLeft: -popupMarginLeft + 'px'/*, marginTop: -popupMarginTop+'px'*/});
-    $('.popup-close, .popup').click(function() {
-        $('.popup').fadeOut();
-    });
-    $('.popup-content').click(function() {
-        return false;
-    });
+$('.popup-content').css({marginLeft: -popupMarginLeft + 'px'/*, marginTop: -popupMarginTop+'px'*/});
+$('.popup-close, .popup').click(function() {
+    $('.popup').fadeOut();
+});
+$('.popup-content').click(function() {
+    return false;
+});
 }
